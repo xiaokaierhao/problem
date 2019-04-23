8ce499491a60a35bccfad15b949747fe94815043
xiaokai <lkrexe@qq.com>
2019-04-23 22:13:08 +0800

    虽然做了伪随机，但后面分数什么的都算不了

--- js/ajax.js
+++ js/ajax.js
@@ -6,18 +6,100 @@ $(function() {
     url:'./quizzes.json',
     success:function (res) {
       var resDate = [];
-      // console.log('aaa');
       resDate = res;
+      var ran = parseInt(Math.random()*res.length);
+      var promblem = new Array();
+      var score = 0;
+      var isCommit = false;
+      for(var i = 0;i<10;i++){
+        promblem[i]=res[ran+i];
+        // console.log(promblem[i]);
+      }
 
-      console.log(resDate);
+
+      $('.stGame').on('click',function(){
+        $('.start').fadeOut(500);
+
+        setTitle(0);
+        setAnswer(0);
+        tanswer(0);
+      })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      function setTitle(a) {
+        $('.title').html(`<h2>${promblem[a].quiz}</h2>`);
+        }
+
+      function setAnswer(a) {
+        for(var i = 0;i<promblem[a].options.length;i++){
+          $('.answer').append(`<p class='item' name = '${i+1}'>${i+1}、${promblem[a].options[i]}</p>`);
+        }
+      }
+      function tanswer(a) {
+        $('.answer').on('click',function (e) {
+          // if(!isCommit){
+          //   isCommit = true;
+          //   $('.answer').fadeOut(function () {
+          //     console.log('asdf');
+          //     isCommit=false;
+          // },1000)
+          // }
+
+          var item = e.target;
+          var no = $(item).attr('name');
+          if(no == promblem[a].answer){
+            // score++;
+            // console.log(no);
+            // $(item).css('background','skyblue');
+          }else{
+            // console.log(no);
+          }
+          if(a<9){
+          setTitle(a+1);
+          $('.answer').html('');
+          setAnswer(a+1);
+          tanswer(a+1);
+          // console.log('asdf');
+        }else{
+          $('.gaming').fadeOut(500);
+
+        }
+
+        })
+      }
+
+
+
+
+     $('.reGame').on('click',function () {
+       window.location.reload();
+     })
+
+
+
+
+
+
+
+
+
 
     }
   })
-  console.log(resDate);
-  res={
-    resDate:resDate,
-    asdf:'asdf'
-  }
+
 
   // function () {
   //   window.res={
--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(function () {
-  console.log(res.asdf);
-  console.log(res.resDate);
+  // console.log(res.asdf);
+  // console.log(res.resDate);
+  // var asdf = new Array();
+  // var len = asdf.length()
+  var he = $(window).height();
+  $('body').css('height',he);
+  console.log(he);
 })
